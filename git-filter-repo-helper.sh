2a3a7f12dc37774cc53b2f54761ef5a81d9c9cc4
oren12321 <oren12321@gmail.com>
2023-05-29 00:14:07 +0300

    Add --dry-run option to script and remove script processing logs

--- git-filter-repo-helper.sh
+++ git-filter-repo-helper.sh
@@ -4,50 +4,58 @@ set -Eeuo pipefail
 [ -z "${DEBUG:-}" ] && DEBUG=0
 [ "${DEBUG}" -eq 1 ] && set -x
 
+usage() {
+    echo "./git-filter-repo-helper.sh <git-repository-directory> <filtered-paths-file> [--dry-run]"
+}
 
 GIT_REPO_DIR="${1:-<unbound>}"
 PATHS_FILTER_FILE="${2:-<unbound>}"
+DRY_RUN="${3:-<unbound>}"
 
 if [ "${GIT_REPO_DIR}" == "<unbound>" ]; then
     echo "Git reposotory argument is required"
+    usage
     exit 1
 fi;
 
 if [ ! -d "${GIT_REPO_DIR}/.git" ]; then
     echo "Directory ${GIT_REPO_DIR} does not contain git reposotory"
+    usage
     exit 1
 fi;
 GIT_REPO_DIR="$(pwd)/${GIT_REPO_DIR}"
 
 if [ "${PATHS_FILTER_FILE}" == "<unbound>" ]; then
     echo "Paths filter file for git-filter-repo is required"
+    usage
     exit 1
 fi;
 
 if [ ! -f "${PATHS_FILTER_FILE}" ]; then
     echo "${PATHS_FILTER_FILE} is not a file"
+    usage
     exit 1
 fi;
 PATHS_FILTER_FILE="$(pwd)/${PATHS_FILTER_FILE}"
 
-echo "This script changes the commit history of the Git reposotory"
-echo "  and removes its tags"
+if [ "${DRY_RUN}" != "<unbound>" ] && [ "${DRY_RUN}" != "--dry-run" ]; then
+    echo "Third argument should be empty or --dry-run"
+    usage
+    exit 1
+fi;
 
 WORK_DIR=`mktemp -d`
-echo "Created temporary working directory ${WORK_DIR}"
 
-pushd "${GIT_REPO_DIR}"
-git remote remove origin
-git tag | xargs git tag -d
-popd
-echo "Removed Git repository origin and tags"
+if [ "${DRY_RUN}" == "<unbound>" ]; then
+    pushd "${GIT_REPO_DIR}" > /dev/null
+    git remote remove origin
+    git tag | xargs git tag -d
+    popd > /dev/null
+fi;
 
 touch "${WORK_DIR}/git_log_results.txt"
-echo "Created ${WORK_DIR}/git_log_results.txt"
 
-echo "Processing commits history..."
-
-pushd "${GIT_REPO_DIR}"
+pushd "${GIT_REPO_DIR}" > /dev/null
 while read FILTERED_PATH; do
     # List files in ${FILTERED_PATH} including hidden files to ${WORK_DIR}/filtered_files
     shopt -s dotglob
@@ -64,7 +72,7 @@ while read FILTERED_PATH; do
     done < "${WORK_DIR}/filtered_files"
     
 done < "${PATHS_FILTER_FILE}" > "${WORK_DIR}/PRESERVED"
-popd
+popd > /dev/null
 
 # filter unique files to preserve
 mv "${WORK_DIR}/PRESERVED" "${WORK_DIR}/PRESERVED_TEMP"
@@ -72,13 +80,16 @@ awk '!a[$0]++' "${WORK_DIR}/PRESERVED_TEMP" > "${WORK_DIR}/PRESERVED"
 
 sort -o "${WORK_DIR}/PRESERVED" "${WORK_DIR}/PRESERVED"
 
-echo "Done processing commits history"
+if [ "${DRY_RUN}" == "--dry-run" ]; then
+    cat "${WORK_DIR}/PRESERVED"
+    rm -rf "${WORK_DIR}"
+    exit 0
+fi;
 
-pushd "${GIT_REPO_DIR}"
+pushd "${GIT_REPO_DIR}" > /dev/null
 git filter-repo --paths-from-file "${WORK_DIR}/PRESERVED" --force --replace-refs delete-no-add
-echo "Ran git-filter-repo"
-popd
+popd > /dev/null
 
 # cleaning
 rm -rf "${WORK_DIR}"
-echo "Removed working directory"
+
